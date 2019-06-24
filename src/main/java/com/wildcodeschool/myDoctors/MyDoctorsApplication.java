e2398a28da56ddd8e2358572f311bc727921844b
Rémy Trohel <remy.trohel@gmail.com>
2019-06-24 13:45:00 +0200

    import Mapping removed

--- src/main/java/com/wildcodeschool/myDoctors/MyDoctorsApplication.java
+++ src/main/java/com/wildcodeschool/myDoctors/MyDoctorsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
